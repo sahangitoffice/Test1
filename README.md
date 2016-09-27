d2807bb84dfb4bf6fb3fbdeb1f1d505c744c7949
sahangitoffice <sahan@hsenidlanka.com>
2016-09-27 15:24:00 +0530

    https://github.com/sahangitoffice/Test1.git

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# hello-world
+first-repo
+=======
+# Test1
+>>>>>>> 3cda2f7f8aeb42e7bf49751bc6b0af89c7e5b554
---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.git;
+
+/**
+ * Created by hsenid on 9/27/16.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("my name");
+    }
+}
