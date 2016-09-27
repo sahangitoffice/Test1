0171f94e002f23484110524cdb25afe95e23107f
sahangitoffice <sahan@hsenidlanka.com>
2016-09-27 16:32:17 +0530

    test_2

---
+++ src/main/java/com/test2/TestMe.java
@@ -0,0 +1,10 @@
+package com.test2;
+
+/**
+ * Created by hsenid on 9/27/16.
+ */
+public class TestMe {
+    public static void main(String[] args) {
+        System.out.println("print");
+    }
+}
